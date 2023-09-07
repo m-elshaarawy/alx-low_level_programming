119a2d1564c0ded3ef364716d351c6477773cf2b
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-08 00:46:46 +0300

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that uses write to print
+ *
+ * Return: 1
+*/
+
+int main(void)
+{
+	char buf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, buf, 59);
+
+	return (1);
+}
