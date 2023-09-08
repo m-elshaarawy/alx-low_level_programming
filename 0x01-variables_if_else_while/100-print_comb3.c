81f9c6992031346131eaac9492c7aecae422f4ff
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-08 16:19:43 +0300

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,23 +12,31 @@ int main(void)
 {
 	int ch = 0 + '0';
 	int ch2 = 1 + '0';
+	int ch3 = 2 + '0';
 
-	while (ch <= '8')
+	while (ch <= '7')
 	{
 
 		putchar(ch);
 		putchar(ch2);
+		putchar(ch3);
 
 		if (ch != '8')
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		ch2++;
-		if (ch2 == 58)
+		ch3++;
+		if (ch3 == 58)
 		{
-			ch++;
-			ch2 = ch + 1;
+			ch2++;
+			ch3 = ch + 1;
+			if (ch2 == '9')
+			{
+				ch++;
+				ch2 = ch + 1;
+				ch3 = ch2 + 1;
+			}
 		}
 	}
 	putchar('\n');
