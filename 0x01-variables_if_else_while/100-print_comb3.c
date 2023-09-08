40786df2bbf992628c67d10d0956809093a62009
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-08 16:26:35 +0300

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,31 +12,25 @@ int main(void)
 {
 	int ch = 0 + '0';
 	int ch2 = 1 + '0';
-	int ch3 = 2 + '0';
 
-	while (ch <= '7')
+	while (ch <= '8')
 	{
 
 		putchar(ch);
 		putchar(ch2);
-		putchar(ch3);
 
 		if (ch != '8')
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		ch3++;
-		if (ch3 == 58)
+
+		ch2++;
+
+		if (ch2 == 58)
 		{
-			ch2++;
-			ch3 = ch + 1;
-			if (ch2 == '9')
-			{
-				ch++;
-				ch2 = ch + 1;
-				ch3 = ch2 + 1;
-			}
+			ch++;
+			ch2 = ch + 1;
 		}
 	}
 	putchar('\n');
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print different combinations of two digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch = 0 + '0';
+	int ch2 = 1 + '0';
+	int ch3 = 2 + '0';
+
+	while (ch <= '7')
+	{
+
+		putchar(ch);
+		putchar(ch2);
+		putchar(ch3);
+
+		if (ch != '8')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		ch3++;
+		if (ch3 == 58)
+		{
+			ch2++;
+			ch3 = ch + 1;
+			if (ch2 == '9')
+			{
+				ch++;
+				ch2 = ch + 1;
+				ch3 = ch2 + 1;
+			}
+		}
+	}
+	putchar('\n');
+
+return (0);
+}
