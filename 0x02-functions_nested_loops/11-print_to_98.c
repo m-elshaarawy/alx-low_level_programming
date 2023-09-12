f955ff370cfb00a80efab1e6f7623e1a6c3b3d38
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-12 21:14:30 +0300

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ *  followed by a new line.
+ * @n:number to start from
+*/
+
+void print_to_98(int n)
+{
+	int i = 0, s = 0, d = 0;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(n + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			_putchar(n + '0');
+		}
+	}
+
+
+	_putchar('\n');
+
+}
