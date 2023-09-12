2cbb335695c22afadb9c2b001378a97791cf9028
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-12 20:36:49 +0300

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,14 +10,27 @@ void times_table(void)
 {
 	int i = 0;
 	int j = 0;
+	int t = 0;
 
 	while (i < 10)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar((i * j) + '0');
+			t = i * j;
+			if (t > 9)
+			{
+				_putchar((t / 10) + '0');
+				_putchar((t % 10) + '0');
+			}
+			else
+			{
+				_putchar((t) + '0');
+			}
 			if (j != 9)
-			_putchar(',');
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 		i++;
