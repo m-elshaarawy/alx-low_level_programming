33df9cf33511f9573d739955c27ae85b643c0db3
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-12 22:55:10 +0300

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,14 +12,16 @@ int main(void)
 {
 	unsigned long num1 = 1, num2 = 2, num3 = 0, i = 0;
 
-	for (i = 0; i < 50; i++)
+	printf("%lu, %lu, ", num1, num2);
+
+	for (i = 0; i < 48; i++)
 	{
 		num3 = num1 + num2;
 
 		printf("%lu", num3);
 		num1 = num2;
 		num2 = num3;
-		if (i != 49)
+		if (i != 47)
 			printf(", ");
 		else
 			printf("\n");
