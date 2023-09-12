2ad319da84785833646957c1c454a7147c5f9e79
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-12 18:28:57 +0300

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if care is in lowercase
+ *
+ * @c : the char that we check
+ * Return: 1 if c is lowercase 0 otherwise
+*/
+
+int print_sign(int n);
