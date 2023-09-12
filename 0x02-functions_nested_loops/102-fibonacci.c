3562c87a29bde22787009b554462edba2dba7693
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-12 22:48:00 +0300

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Description: print fibonacci
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int num1 = 1, num2 = 2, num3 = 0, i = 0;
+
+	for (i = 0; i < 50; i++)
+	{
+		num3 = num1 + num2;
+
+		printf("%d", num3);
+		num1 = num2;
+		num2 = num3;
+		if (i != 49)
+			printf(", ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
