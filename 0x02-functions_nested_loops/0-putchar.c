d6a752861a3bab385a7300f1631f6abdca62a546
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-12 15:25:11 +0300

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
