8e678f904c5a759aaf20171afec48845293477d2
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-12 22:30:47 +0300

    :)

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,15 +29,18 @@ void print_times_table(int n)
 			if (t <= 9)
 			{
 				_putchar(' ');
-
 			}
-			else
+			if (t <= 99)
 			{
-				_putchar((t / 10) + '0');
-
+				_putchar(' ');
 			}
+			if (t >= 100)
+			{
+				_putchar((t / 100) + '0');
+				_putchar((t / 10) % 10 + '0');
+			} else if (t <= 99 && t >= 10)
+			_putchar((t / 10) + '0');
 			_putchar((t % 10) + '0');
-
 		}
 		_putchar('\n');
 		i++;
