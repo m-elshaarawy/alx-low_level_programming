e5871c3b34e5e247431db66594a80cb6816ea286
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-14 16:27:58 +0300

    5. Numbers constitute the only universal language0.4

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,9 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 15; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 15; j++)
 		{
 			if (j > 9)
 				_putchar((j / 10) + '0');
