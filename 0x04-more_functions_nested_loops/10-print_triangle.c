43a81279bb631b7543acf7210810eeae1f64270f
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-14 18:07:41 +0300

    10. Triangles0.1

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,14 +16,14 @@ void print_triangle(int size)
 		return;
 	}
 
-	for (i = 0; i < size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = size - 1; j > 0; j--)
+		for (j = i; j < size; j++)
 		{
-			_putchar('#');
+			_putchar(' ');
 		}
 
-		for (j = i; j < size; j++)
+		for (j = 1; j <= i; j++)
 		{
 
 			_putchar('#');
