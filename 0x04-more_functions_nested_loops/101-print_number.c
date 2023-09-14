e955f622ded03394f2152330bbd8feb206e99cdb
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-14 21:00:12 +0300

    12. Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,11 +12,7 @@ void print_number(int n)
 		_putchar('-');
 	}
 
-	if (n >= 1000)
-		_putchar((n / 1000) + '0');
-	if (n >= 100)
-		_putchar((n / 100) + '0');
-	if (n >= 10)
-		_putchar((n / 10) + '0');
+	if (n > 10)
+		print_number((n / 10) + '0');
 	_putchar((n % 10) + '0');
 }
