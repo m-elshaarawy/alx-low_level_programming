e7c2031cf9e4af8a5bd5664837a66a23544a7b81
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-25 18:36:55 +0300

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: input string
+ * @c: input char
+ * Return: pointer to char
+*/
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return ('\0');
+}
