12c516923437c71e687a6d73737bdb933fa6b78b
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-09-28 11:31:38 +0300

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,9 +9,12 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return ('\n');
+		_putchar('\n');
 	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+	else
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
 }
