8a8234a241c286dc356d9db83480a39337d75633
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-10-31 21:48:22 +0200

    3. If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: number of columns
+ * @height: number of rows
+ * Return: 2d arr
+*/
+
+
+int **alloc_grid(int width, int height)
+{
+	int **graph, i = 0;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	graph = malloc(sizeof(*graph) * height);
+
+	for (i = 0; i < height ; i++)
+	{
+		graph[i] = calloc(sizeof(int), 6);
+
+		if (graph[i] == 0)
+		{
+			while (i--)
+			{
+				free(graph[i]);
+			}
+			return (NULL);
+		}
+	}
+	return (graph);
+}
