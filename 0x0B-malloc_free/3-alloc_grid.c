5e4b2584efbf4c0aa9c2e59d39df729e70f72181
m_elshaaraw <m.elshaarawy96@yahoo.com>
2023-10-31 21:55:32 +0200

    3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height ; i++)
 	{
-		graph[i] = calloc(sizeof(**graph), width);
+		graph[i] = calloc(sizeof(int), width);
 
 		if (graph[i] == 0)
 		{
