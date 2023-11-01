31df37d16372a099998a6c1d8405dcd128ac8bc0
m.elshaarawy <mohamed.elshaarawy@redsea.com>
2023-11-01 03:47:30 -0700

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -26,19 +26,21 @@ char *argstostr(int ac, char **av)
 
 	if (str == 0)
 		return (NULL);
-	
-	for (i = 0; i < ac ; i++)
+
+	for (i = 0 ; i < ac ; i++)
 	{
-		for (j = 0; av[i][j] ; j++, z++)
+		for (j = 0 ; av[i][j] ; j++, z++)
 		{
 			str[z] = av[i][j];
 		}
+
 		str[z] = '\n';
 		z++;
 	}
-	str[z] = '\0';
-	return (str);
 
+	str[z] = '\0';
+
+	return (str);
 }
 
 /**
@@ -49,12 +51,12 @@ char *argstostr(int ac, char **av)
 
 int _strlen(char *s)
 {
-        int i;
+	int i;
 
-        if (s == 0)
-                return (0);
+	if (s == 0)
+	return (0);
 
-        for (i = 0; s[i]; i++)
-        {}
-        return (i);
+	for (i = 0; s[i]; i++)
+	{}
+	return (i);
 }
