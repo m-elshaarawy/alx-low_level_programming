2d760fa683b8b26f4c73dad115ec3275ea187c52
m.elshaarawy <mohamed.elshaarawy@redsea.com>
2023-11-02 06:15:18 -0700

    3. array_range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers.
+ * @min: min number
+ * @max: max number
+ * Return: Pionter
+ */
+
+int *array_range(int min, int max)
+{
+	int *p;
+	int size = 0, i = 0;
+
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+
+	p = mallc(sizeof(int) * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		p[i] = min++;
+
+	return (p);
+}
