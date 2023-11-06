811b46489eefe5160242f50a8a5f3366a1f5b26c
m.elshaarawy <mohamed.elshaarawy@redsea.com>
2023-11-06 01:46:06 -0800

    3. Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef _FUNCTION_LIKE_MICRO_H
 #define _FUNCTION_LIKE_MICRO_H
 
-#define ABS(x) ((x < 0) ? (x * -1) : (x))
+#define ABS(x) (((x) < 0) ? ((x) * -1) : (x))
 
-#endif /* #define ABS(x) ((x < 0) ? (x * -1) : (x)) */
+#endif /* _functio_like_micro_H  */
 
