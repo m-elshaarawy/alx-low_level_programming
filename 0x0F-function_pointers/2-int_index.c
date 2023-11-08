a693912474ca8db0986cd5806683b4c44dd9d5c0
m.elshaarawy <mohamed.elshaarawy@redsea.com>
2023-11-08 06:19:58 -0800

    2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer.
+ * @array: arr
+ * @size: size
+ * @cmp: pointer to function
+ * Description: N/A
+ * Return: void
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i = 0;
+
+	if (array && size && cmp)
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+
+	return (-1);
+}
