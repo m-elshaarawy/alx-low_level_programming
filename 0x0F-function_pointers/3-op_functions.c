05970e4b4d605e13424a2e0a69493d092e27c0b8
m.elshaarawy <mohamed.elshaarawy@redsea.com>
2023-11-08 07:22:29 -0800

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - the sum of a and b.
+ * @a: num1
+ * @b: num2
+ * Descripion: N/A
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of a and b.
+ * @a: num1
+ * @b: num2
+ * Descripion: N/A
+ * Return: subtraction
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of a and b
+ * @a: var 1
+ * @b: var 2
+ * Return: result
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - main 4
+ *
+ * @a: var 1
+ * @b: var 2
+ *
+ * Return: 0
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - main 5
+ *
+ * @a: var 1
+ * @b: var 2
+ *
+ * Return: result
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
