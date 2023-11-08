86e5d3c006013ae57539fdafd28915ce6b066d1c
m.elshaarawy <mohamed.elshaarawy@redsea.com>
2023-11-08 07:28:00 -0800

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -1,19 +1,18 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <string.h> /*for atoi*/
-#include <stdlib.h> /*for exit*/
 
 /**
- * main - main func
- * @argc: var 1
- * @argv: var 2
+ * main - main function
+ * @argc: The argument count.
+ * @argv: The argument vector.
  *
- * Return: 0
-*/
+ * Description: This program perform simple operations.
+ *
+ * Return: 0.
+ */
+
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *operator;
+	int num1, num2, process;
 
 	if (argc != 4)
 	{
@@ -23,19 +22,27 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	operator = argv[2];
-	if (get_op_func(operator) == NULL || operator[1] != '\0')
+
+	if (argv[2][1] != 0 ||
+	    (argv[2][0] != '+' &&
+	    argv[2][0] != '-' &&
+	    argv[2][0] != '*' &&
+	    argv[2][0] != '/' &&
+	    argv[2][0] != '%'))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*operator == 47 || *operator == 37) && num2 == 0)
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(operator)(num1, num2));
-
+	process = (*get_op_func(argv[2]))(num1, num2);
+	{
+		printf("%d\n", process);
+	}
 	return (0);
 }
